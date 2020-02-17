f0ada63c6acdb6e8e1042c2db6fd22c049a18d00
Data Acquisition <data_acquisition@WDANHI003.ist.local>
2020-02-17 16:33:39 +0100

    xpoints and float formatting

--- qcodes_contrib_drivers/drivers/SignalHound/USB_SA124B.py
+++ qcodes_contrib_drivers/drivers/SignalHound/USB_SA124B.py
@@ -34,7 +34,7 @@ class SpectrumArray(ParameterWithSetpoints):
 
     def get_raw(self):
         # :TRACe[:DATA]? need to test this live
-        npoints = self.root_instrument.n_points.get_latest()
+        npoints = self.root_instrument.x_points.get_latest()
         return np.random.rand(npoints)
 
 
@@ -57,48 +57,55 @@ class USB_SA124B(VisaInstrument):
                            label='f start',
                            vals=Numbers(0,13e9),
                            get_cmd=":SENSe:FREQuency:STARt?",
-                           set_cmd=":SENSe:FREQuency:STARt {}" )
+                           set_cmd=":SENSe:FREQuency:STARt {}",
+                           get_parser=float )
 
         self.add_parameter('f_stop',
                            unit='Hz',
                            label='f stop',
                            vals=Numbers(0,13e9),
                            get_cmd=":SENSe:FREQuency:STOP?",
-                           set_cmd=":SENSe:FREQuency:STOP {}" )
+                           set_cmd=":SENSe:FREQuency:STOP {}",
+                           get_parser=float )
 
         self.add_parameter('f_center',
                         unit='Hz',
                         label='f center',
                         vals=Numbers(0,13e9),
                         get_cmd=":SENSe:FREQuency:CENTER?",
-                        set_cmd=":SENSe:FREQuency:CENTER {}" )
+                        set_cmd=":SENSe:FREQuency:CENTER {}",
+                        get_parser=float )
 
         self.add_parameter('f_span',
                         unit='Hz',
                         label='f span',
                         vals=Numbers(0,13e9),
                         get_cmd=":SENSe:FREQuency:SPAN?",
-                        set_cmd=":SENSe:FREQuency:SPAN {}" )
+                        set_cmd=":SENSe:FREQuency:SPAN {}",
+                        get_parser=float )
 
         # x_ are used for computing trace x spacing
         self.add_parameter('x_start',
                         unit='',
                         label='x start',
                         get_cmd=":TRACe:XSTARt?",
-                        set_cmd="" )
+                        set_cmd="",
+                        get_parser=float )
 
         self.add_parameter('x_inc',
                         unit='',
                         label='x increment',
-                        get_cmd=":TRACe:XSTARt?",
-                        set_cmd="" )
+                        get_cmd=":TRACe:XINCrement?",
+                        set_cmd="",
+                        get_parser=float )
 
         self.add_parameter('x_points',
                            unit='',
                            initial_value=10,
                            vals=Numbers(1,1e9),
                            get_cmd=":TRACe:POINts?",
-                           set_cmd="")
+                           set_cmd="",
+                           get_parser=int )
 
         self.add_parameter('freq_axis',
                            unit='Hz',
@@ -107,11 +114,11 @@ class USB_SA124B(VisaInstrument):
                            startparam=self.x_start,
                            incparam=self.x_inc,
                            xpointsparam=self.x_points,
-                           vals=Arrays(shape=(self.n_points.get_latest,)))
+                           vals=Arrays(shape=(self.x_points.get_latest,)))
 
         self.add_parameter('spectrum',
                    unit='dBm',
                    setpoints=(self.freq_axis,),
                    label='Noise power',
                    parameter_class=SpectrumArray,
-                   vals=Arrays(shape=(self.n_points.get_latest,)))
+                   vals=Arrays(shape=(self.x_points.get_latest,)))
