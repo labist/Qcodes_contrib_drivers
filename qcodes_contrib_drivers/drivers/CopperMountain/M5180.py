562fa59bf19a54ffc02e0041d630ffab0cfe7f16
Endeavour_Data_Computer <jorden.senior@ist.ac.at>
2020-10-12 12:06:34 +0200

    update power range for copper mountain M5180

--- qcodes_contrib_drivers/drivers/CopperMountain/M5180.py
+++ qcodes_contrib_drivers/drivers/CopperMountain/M5180.py
@@ -4,7 +4,7 @@ class M5180(M51xx.CMTxBase):
     def __init__(self, name, address, **kwargs):
         super().__init__(name, address,
                          min_freq=3e5, max_freq=18e9,
-                         min_power=-30, max_power=13,
+                         min_power=-50, max_power=10,
                          nports=1,
                          **kwargs)
 
