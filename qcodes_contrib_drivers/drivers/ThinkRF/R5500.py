4c9a8f6077bac62de581e3c106e76c74a15422c1
Data Acquisition <data_acquisition@WDANHI003.ist.local>
2022-04-07 19:32:53 +0200

    add test scripts for sweep sa

--- qcodes_contrib_drivers/drivers/ThinkRF/R5500.py
+++ qcodes_contrib_drivers/drivers/ThinkRF/R5500.py
@@ -4,7 +4,7 @@ from pyrf.devices.thinkrf import WSA
 from pyrf.sweep_device import SweepDevice
 from pyrf.util import capture_spectrum
 from pyrf.util import compute_usable_bins
-from pyrf.sweep_device import SweepDevice
+from pyrf.sweep_device_dpmod import SweepDevice
 
 # qcodes imports
 from qcodes.instrument.base import Instrument
@@ -27,7 +27,11 @@ class Setpoints(Parameter):
         self._npointspar = npointspar
 
     def get_raw(self):
-        npts = self._npointspar()
+        sa = self.root_instrument
+        if sa._acquired_data is not None:
+            npts = sa._acquired_data['npts']
+        else:
+            npts = self._npointspar()
         start = self._startpar()
         stop = self._stoppar()
        
@@ -39,20 +43,10 @@ class SpectrumArray(ParameterWithSetpoints):
     Generates an array of noise spectra data with 
     frequency setpoints using capture_sweep_device
     '''
-    def __init__(   self, 
-                    name: str,
-                    instrument: 'R5500',
-                    label: str,
-                    unit: str,
-                    **kwargs    ) -> None:
-        super().__init__(   name, instrument=instrument,
-                            label=label, unit=unit, 
-                            **kwargs)
-
     def get_raw(self):
         sa = self.root_instrument
-        sa.get_npoints()
-        return sa._acquired_data['spectra']
+        sa.n_points()
+        return sa._acquired_data['spectrum']
 
 
 class R5500(Instrument):
@@ -64,24 +58,21 @@ class R5500(Instrument):
         super().__init__(name, **kwargs)
         
         self.dut = WSA()
+        self.addr = address
         self.dut.connect(address)
         self.dut.request_read_perm()
 
-        self.sweepdevice = SweepDevice(self.dut)
-
         # Basic settings
-        self._rfe_mode = 'SH'
-        self._f_start = 6e9
-        self._f_stop = 50e6
+        self._rfemode = 'SH'
+        self._fstart = 5e9
+        self._fstop = 6e9
         self._rbw = 5e3
         self._gain = 'high'
         self._attenuation = 0
-        self._average = 1
+        self._average = 10
         self._decimation = 1
         self._reflevel = 0
-
-        self._freqlist = []
-        self._spectralist = []
+        self.triggered = False
 
         self._acquired_data = None
 
@@ -89,52 +80,52 @@ class R5500(Instrument):
                                 unit = '',
                                 initial_value= 'SH',
                                 label = 'Input Mode',
-                                get_cmd = self.dut.rfe_mode,
-                                set_cmd = self.dut.rfe_mode,
+                                get_cmd = self.get_rfe_mode,
+                                set_cmd = self.set_rfe_mode,
                                 get_parser = str)
         
         self.add_parameter('attenuation',
                                 unit = 'dB',
                                 initial_value = 0.0,
                                 label = 'attenuation',
-                                get_cmd = self.dut.attenuator,
-                                set_cmd = self.dut.attenuator,
+                                get_cmd = self.get_attenuation,
+                                set_cmd = self.set_attenuation,
                                 get_parser = float,)
 
         self.add_parameter('gain',
                                 unit = '',
                                 label = 'gain',
-                                get_cmd = self.dut.psfm_gain,
-                                set_cmd = self.dut.psfm_gain,
+                                get_cmd = self.get_psfm_gain,
+                                set_cmd = self.set_psfm_gain,
                                 get_parser = str)
         
         self.add_parameter('average',
                                 unit = '',
                                 label = 'average',
-                                get_cmd = self.average,
-                                set_cmd = self.average,
+                                get_cmd = self.get_average,
+                                set_cmd = self.set_average,
                                 get_parser = int)
 
         self.add_parameter('rbw',
                                 unit = 'Hz',
                                 label = 'resolution bandwidth',
-                                get_cmd = self.rbw,
-                                set_cmd = self.rbw ,
+                                get_cmd = self.get_rbw,
+                                set_cmd = self.set_rbw ,
                                 get_parser = float)
 
 
         self.add_parameter('f_start',
                                 unit='Hz',
                                 label='fstart',
-                                get_cmd= self.f_start,
-                                set_cmd= self.f_start,
+                                get_cmd= self.get_fstart,
+                                set_cmd= self.set_fstart,
                                 get_parser = float)
 
         self.add_parameter('f_stop',
                                 unit='Hz',
                                 label='fstop',
-                                get_cmd = self.f_stop,
-                                set_cmd= self.f_stop,
+                                get_cmd = self.get_fstop,
+                                set_cmd= self.set_fstop,
                                 get_parser = float)
 
         self.add_parameter('n_points',
@@ -147,9 +138,9 @@ class R5500(Instrument):
                                 unit='Hz',
                                 label='Frequency',
                                 parameter_class=Setpoints,
-                                startparam=self.f_start,
-                                stopparam=self.f_stop,
-                                xpointsparam=self.n_points,
+                                startpar=self.f_start,
+                                stoppar=self.f_stop,
+                                npointspar=self.n_points,
                                 vals=Arrays(shape=(self.n_points.get_latest,)))
 
         self.add_parameter('spectrum',
@@ -162,17 +153,25 @@ class R5500(Instrument):
 
     def get_npoints(self):
         '''
-
+        Configs the sweep and collects the data. Returns length of data for
+        generating the setpoints.
         '''             
-        fstart = self.fstart
-        fstop = self.fstop
-        rbw = self.rbw
-        device_settings = {'attenuator' : self._attenuation}
-        mode = self.rfe_mode
-        average = self.average
+        fstart = self.f_start()
+        fstop = self.f_stop()
+        rbw = self.rbw()
+        device_settings = { 'attenuator' : self.attenuation() }
+        mode = self.rfe_mode()
+        average = self.average()
+
+        self.dut.reset()
+        self.dut.psfm_gain(self._gain)
+        self.dut.spp(1024)
+        self.dut.ppb(4)
+        self.dut.pll_reference('EXT')
 
         sweepdev = SweepDevice(self.dut)
 
+        sweepdev.real_device.flush_captures()
         fstart, fstop, spectrum = sweepdev.capture_power_spectrum(fstart=fstart,
                                fstop=fstop,
                                rbw=rbw,
@@ -182,34 +181,60 @@ class R5500(Instrument):
         
         self._acquired_data = dict({'fstart':fstart,
                                 'fstop' : fstop,
-                                'spectrum' : spectrum})
+                                'spectrum' : spectrum,
+                                'npts' : len(spectrum) })
         self.f_start(fstart)
         self.f_stop(fstop)
 
-        return (len(spectrum),)
+        self.dut.sweep_stop()
+        self.dut.abort()
+        self.dut.flush_captures()
+
+        print("INVOKED !")
+
+        return len(spectrum)
                     
-    @property
-    def f_start( self, f_start=None ):
-        if f_start is None:
-            return self._f_start
-        self._f_start = f_start
+    def get_fstart( self ):
+        return self._fstart
 
-    @property
-    def f_stop( self, f_stop=None ):
-        if f_stop is None:
-            return self._f_stop
-        self._f_start = f_stop
+    def set_fstart( self, fstart ):
+        self._fstart = fstart
 
-    @property
-    def average( self, average = None ):
-        if average is None:
-            return self._average
+    def get_fstop( self ):
+        return self._fstop
+        
+    def set_fstop( self, fstop ):
+        self._fstop = fstop
+
+    def get_average( self ):
+        return self._average
+
+    def set_average( self, average ):
         self._average = average
 
-    @property
-    def rbw(self, rbw = None):
-        if rbw is None:
-            return self._rbw
+    def get_rbw( self ):
+        return self._rbw
+
+    def set_rbw(self, rbw):
         self._rbw = rbw
 
+    def get_rfe_mode( self ):
+        return self._rfemode
+    
+    def set_rfe_mode( self, rfemode ):
+        self._rfemode = rfemode
+        self.dut.rfe_mode(self._rfemode)
 
+    def get_attenuation( self ):
+        return self._attenuation
+
+    def set_attenuation( self, atten ):
+        self._attenuation = atten
+        self.dut.attenuator( self._attenuation )
+
+    def get_psfm_gain( self ):
+        return self._gain
+
+    def set_psfm_gain( self, gain ):
+        self._gain = gain
+        self.dut.psfm_gain( self._gain )
