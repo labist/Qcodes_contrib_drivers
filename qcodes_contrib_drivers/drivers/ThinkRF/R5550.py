bf06b4bbc68016a06d4fab7ab2fb739d1376c8b1
Voyager Data Acquisition <andrew.higginbotham@ist.ac.at>
2023-03-28 09:24:33 +0200

    parameter names, cleanup

--- qcodes_contrib_drivers/drivers/ThinkRF/R5550.py
+++ qcodes_contrib_drivers/drivers/ThinkRF/R5550.py
@@ -6,18 +6,10 @@ from pyrf.devices.thinkrf import WSA
 from pyrf.sweep_device import SweepDevice
 from pyrf.numpy_util import calculate_channel_power
 
-# TODO: make these into parameters
-# TODO: add a channel_power command
-# TODO: add attenuator
-RBW = 400e3
-# RFE_MODE = 'SH'
-# START_FREQ = 1.39e9
-# STOP_FREQ = 1.41e9
-
 class R5550(Instrument):
     """ R5550 insturment class. Wraps the capture_power_spectrum method provided by thinkrf
     """
-    def __init__(self, name, address, start=1e6, stop=10e9, navg=1, rfe='SH', rbw=400e3, attenuator=0 ):
+    def __init__(self, name, address, start=1e6, stop=10e9, averages=1, rfe='SH', rbw=400e3, attenuator=0 ):
         """
         Create R5550
         Args:
@@ -25,7 +17,7 @@ class R5550(Instrument):
             address: ip address
             start: start frequency in Hz
             stop: stop frequency in Hz
-            navg: number of averages
+            averages: number of averages
             rfe: one of SH, SHN, ..
             rbw: resolution bandwidth in Hz
             attenuator: attenuator setting in dB
@@ -33,13 +25,9 @@ class R5550(Instrument):
 
         super().__init__(name=name)
         self.address = address
-        dut = WSA()
-        dut.connect(self.address)
-        dut.request_read_perm()
-        dut.configure_flattening(loaded=None)
-        self.sd = SweepDevice(dut)
 
-        self._navg = navg
+        self.connect()
+        self._navg = averages
         self.add_parameter('averages', 
                            label='Averages', 
                            unit='#', 
@@ -119,7 +107,7 @@ class R5550(Instrument):
         """ connect to WSA
         """
         dut = WSA()
-        dut.connect(self.ip)
+        dut.connect(self.address)
         dut.request_read_perm()
         dut.configure_flattening(loaded=None)
         self.sd = SweepDevice(dut)
@@ -144,6 +132,7 @@ class R5550(Instrument):
     def get_channel_pwr(self):
         """ calculate channel power from averaged spectrum
         """
-        return calculate_channel_power(self._get_spectrum())
+        return calculate_channel_power(self.spectrum())
 
-# spec = R5550(name='r5550', ip='169.254.16.253')
+if __name__ == "__main__" :
+    spec = R5550(name='r5550', address='169.254.16.253')
